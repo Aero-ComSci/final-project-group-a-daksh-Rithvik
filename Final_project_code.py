b6df9dadafd5f42e621d4e0821fb01d391b7aa03
RithvikJangam <rithjangam@gmail.com>
2025-05-28 11:30:56 -0700

    Update Final_project_code.py

--- Final_project_code.py
+++ Final_project_code.py
@@ -1,1 +1,15 @@
+import requests
+api_key = '30d4741c779ba94c470ca1f63045390a'
+lucky_city = ["los angeles", "dublin", "pleasanton", "miami", "durham")
+city = input("Which City Weather Would You Like To Know: ").lower()
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + api_key)
+data = response.json()
 
+if data['cod'] == '404':
+  print("City Not Found")
+else:
+  print("Weather: " + data['weather'][0]['main'])
+  print("Temperature: " + str(round(data['main']['temp'])) + 'F')
+  if city in lucky_city:
+    print("Congratulations! You are the lucky winner!!!")
+    print("You get a Free Ice Cream")
