0e3649444c833ec87ac11d0eba79a989d4bb8540
RithvikJangam <rithjangam@gmail.com>
2025-05-28 11:37:37 -0700

    Update Final_project_code.py

--- Final_project_code.py
+++ Final_project_code.py
@@ -1,6 +1,6 @@
 import requests
 api_key = '30d4741c779ba94c470ca1f63045390a'
-lucky_city = ["los angeles", "dublin", "pleasanton", "miami", "durham")
+lucky_city = ["los angeles", "dublin", "pleasanton", "miami", "durham"]
 city = input("Which City Weather Would You Like To Know: ").lower()
 response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + api_key)
 data = response.json()
@@ -13,3 +13,4 @@ else:
   if city in lucky_city:
     print("Congratulations! You are the lucky winner!!!")
     print("You get a Free Ice Cream")
+    lucky_city.remove(city)
